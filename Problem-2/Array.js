15caf80ae26ca6142551b8821ae9847a7212fcf4
HusnainHaider21 <husnainhaider0304@gmail.com>
2024-07-29 10:12:26 +0500

    Write two logics to understand Parameters and Arguments

---
+++ Problem-2/Array.js
@@ -0,0 +1,15 @@
+/*Multiplication*/
+function squareArray(number) {
+    return number*number;   
+}
+const result = squareArray(6);
+console.log(result);
+
+/*Addition*/
+function duplicate(nums) {
+    return nums+nums;
+    
+}
+
+const ans = duplicate(44);
+console.log(ans);
